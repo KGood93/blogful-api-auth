5c2203ed4a531127a0393020fe61fd114451ecd8
KGood93 <goodreau.kath@gmail.com>
2019-12-08 15:38:57 -0600

    fix typo

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,5 +1,5 @@
 function requireAuth(req, res, next) {
-    return res.status(401).json({error: 'Missing bearer token'})
+    return res.status(401).json({error: 'Missing bearer token'}),
     next()
 }
 
